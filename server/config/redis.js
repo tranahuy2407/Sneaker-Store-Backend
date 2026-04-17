0a30ac679958eab4288b755c26d429ce181856c6
Tran A Huy <91709513+tranahuy2407@users.noreply.github.com>
2026-04-17 11:25:33 +0700

    fix: Update Redis

--- server/config/redis.js
+++ server/config/redis.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
+  port: parseInt(process.env.REDIS_PORT) || 6379,
   password: process.env.REDIS_PASSWORD || undefined,
-  db: process.env.REDIS_DB || 0,
+  db: parseInt(process.env.REDIS_DB) || 0,
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 2000);
     return delay;
